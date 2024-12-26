601f7da9858845c67baad9a5d138d08430a9bfbf
Evan Ashley <evan.ashley@comcast.net>
2024-12-26 15:51:07 -0800

    Add search user and navigate feature
    This change adds a search user feature which, on click,
    navigates to the user's profile.

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -1,17 +1,39 @@
 import TopNavigation from "@cloudscape-design/components/top-navigation";
-import React, {useCallback, useEffect, useMemo} from "react";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 import {Outlet, useNavigate} from "react-router";
 import {useCookies} from "react-cookie";
 import {SESSION_ID_COOKIE} from "../constant/cookie";
 import {useAuthIdentity} from "../api/auth";
-import {useGetUser} from "../api/user";
-import {ButtonDropdownProps} from "@cloudscape-design/components";
+import {useGetUser, useSearchUsers} from "../api/user";
+import {Autosuggest, AutosuggestProps, ButtonDropdownProps} from "@cloudscape-design/components";
 import '../dayjs';
 import dayjs from 'dayjs';
+import {SearchUsersResponse, User} from "../types/user";
+import {flow} from "lodash";
+import {flatten, map} from "lodash/fp";
+import {NonCancelableCustomEvent} from "@cloudscape-design/components/internal/events";
 
 const Layout = () => {
+    const [usernameSearchString, setUsernameSearchString] = useState<string>('');
     const [cookies, , deleteCookie] = useCookies([SESSION_ID_COOKIE]);
     const navigate = useNavigate();
+
+    const {
+        data: searchUsersResponse,
+    } = useSearchUsers({
+        usernameSearchString,
+        maxPageSize: 5
+    });
+    const users: User[] = useMemo(() =>
+            flow(map<SearchUsersResponse, User[]>(({users}) => users), flatten)(searchUsersResponse?.pages),
+        [searchUsersResponse]);
+    const autosuggestUserOptions: AutosuggestProps.Option[] = useMemo(() => {
+        return users.map(({ id, username }) => ({ value: id, label: username }));
+    }, [users]);
+    const useSelectOption = useCallback(({ detail }: NonCancelableCustomEvent<AutosuggestProps.SelectDetail>) => {
+        setUsernameSearchString('');
+        navigate(`/profile/${detail.value}`);
+    }, [navigate]);
 
     const sessionIdCookie: string | undefined = cookies[SESSION_ID_COOKIE];
     useEffect(() => {
@@ -20,8 +42,8 @@ const Layout = () => {
         }
     }, [navigate, sessionIdCookie]);
 
-    const {data: authIdentity } = useAuthIdentity();
-    const {data: getUserResponse } = useGetUser({
+    const {data: authIdentity} = useAuthIdentity();
+    const {data: getUserResponse} = useGetUser({
         id: authIdentity?.userId ?? ''
     });
     const memberSinceFormattedDate: string | undefined = useMemo(() => {
@@ -65,7 +87,14 @@ const Layout = () => {
                             {id: signOutId, text: "Sign out"}
                         ]
                     }
-                ]}/>
+                ]}
+                search={<Autosuggest
+                    value={usernameSearchString ?? ''}
+                    onChange={({detail}) => setUsernameSearchString(detail.value)}
+                    options={autosuggestUserOptions}
+                    onSelect={useSelectOption}
+                />}
+            />
             <Outlet/>
         </div>
     );
